2f18731ddf3b0677bbaad13d8cdd2d20a8ad4b1d
Thiago souza <thiago_souza_123456@hotmail.com>
2019-10-25 20:52:47 -0300

    commit

---
+++ do/do/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @do
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.Write("\nLAÇO DE REPETIÇAO DO...WHILE");
+            do//faca
+            {
+                Console.Write("\n" + i);
+                i++;
+
+            } while (i <= 10);//enquanto...
+            Console.ReadKey();
+        }
+    }
+}
