e39d86bca1d8d9a52fcd525dcc0a38fa4451bf14
Thiago souza <thiago_souza_123456@hotmail.com>
2019-10-25 21:29:38 -0300

    commit

---
+++ Exemplos vetores/exemplo1/exemplo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[5];//declaração do vetor de 5 posições
+            int i;//contador do for.
+            //inserindo valores no vetor
+            for(i=0; i<5; i++)
+            {
+                Console.Write("Digite o[{0º}] número: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            //mostrando os valores do vetor
+            Console.WriteLine("impressão do vetor: ");
+            for(i=0; i<5;i++)
+            {
+                //os valores são referenciados na memoria ram
+                //como sendo {0} a valor de i e {1} p vaçpr de vetor[i]
+                Console.WriteLine("[{0º}]= {1}", i, vetor[i]);
+            }
+            //finaliza
+            Console.ReadKey();
+        }
+    }
+}
