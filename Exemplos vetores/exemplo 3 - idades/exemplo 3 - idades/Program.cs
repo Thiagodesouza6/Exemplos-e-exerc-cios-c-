0ca617129da4808d887a1faac96e0b73c80adc7b
Thiago souza <thiago_souza_123456@hotmail.com>
2019-10-25 22:01:16 -0300

    commit

---
+++ Exemplos vetores/exemplo 3 - idades/exemplo 3 - idades/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_3___idades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] idade = new int[5];
+            int i;
+
+           
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("Digite a idade: ");
+                idade[i] = int.Parse(Console.ReadLine());
+            }
+           
+            Console.WriteLine("impressão do vetor: ");
+            for (i = 0; i < 5; i++)
+            {
+                
+                Console.WriteLine("Idade: {0} ", idade[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
