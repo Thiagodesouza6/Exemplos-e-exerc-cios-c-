9246cbd9695454e8a8072d4e43d1a50455e7cf26
Thiago souza <thiago_souza_123456@hotmail.com>
2019-10-29 21:52:11 -0300

    commit

---
+++ Exemplos vetores/Exemplo 6 - Ao cubo/Exemplo 6 - Ao cubo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_6___Ao_cubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetor = new double[6];
+            double[] cubo = new double[6];
+            int i;
+           
+
+            for (i = 1; i < vetor.Length; i++)
+            {
+                Console.Write("Digite o {0}º número: ",i);
+                vetor[i] = int.Parse(Console.ReadLine());
+
+            }
+
+
+            Console.WriteLine("impressão do vetor: ");
+            for (i = 1; i < vetor.Length; i++)
+            {
+                cubo[i] = Math.Pow(vetor[i],  3) ;
+
+                Console.WriteLine("{0}º: {1} ", i, cubo[i]);
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
