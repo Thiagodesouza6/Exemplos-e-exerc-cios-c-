a05aef2a76990581e2faf823b9982b31fe15e266
Thiago souza <thiago_souza_123456@hotmail.com>
2019-10-29 21:27:44 -0300

    commit

---
+++ Exemplos vetores/exemplo 4 - invertido/exemplo 4 - invertido/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_4___invertido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]vetor = new int[5];
+            int[] invertido = new int[5];
+            int i;
+
+
+            for (i = 0; i < vetor.Length; i++)
+            {
+                Console.Write("Digite um número: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+                
+            }
+
+            
+            Console.WriteLine("impressão do vetor: ");
+            for (i = 0; i <vetor.Length; i++)
+            {
+                invertido[i] = vetor[vetor.Length - 1 - i];
+                
+                    Console.WriteLine("[{0}º]: {1} ", i, invertido[i]);
+                
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
