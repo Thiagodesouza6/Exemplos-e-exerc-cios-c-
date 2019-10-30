d9b88bfe0a0e35120a1e51290bdde5b03c764853
Thiago souza <thiago_souza_123456@hotmail.com>
2019-10-29 21:42:33 -0300

    commit

---
+++ Exemplos vetores/Exemplo 5 - posição/Exemplo 5 - posição/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_5___posição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[11];
+           
+            int i;
+
+
+            for (i = 1; i < vetor.Length; i++)
+            {
+                Console.Write("Digite a {0}º idade: ",i);
+                vetor[i] = int.Parse(Console.ReadLine());
+
+            }
+
+
+           
+           
+            Console.WriteLine("\n6º idade: {0}  ", vetor[6]);
+
+            Console.ReadKey();
+        }
+    }
+}
