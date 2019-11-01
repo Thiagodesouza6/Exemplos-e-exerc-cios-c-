f6f97e6ab1853ffc5ccca7227e4073aa0c2ea55c
Thiago souza <thiago_souza_123456@hotmail.com>
2019-11-01 19:33:24 -0300

    commit

---
+++ Exercício 2 - soma dos vetores/Exercício 2 - soma dos vetores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_2___soma_dos_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[9];
+
+            int i, soma = 0;
+            for (i = 0; i < vetor.Length; i++)
+            {
+                Console.Write("Digite um número: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+                soma = vetor[i++]+soma;
+            }
+
+            Console.WriteLine("\nA soma é: "+soma);
+         
+            Console.ReadKey();
+        }
+    }
+}
