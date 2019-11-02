bf6d32db5b1be478d39581532e7fd907d7343b86
Thiago souza <thiago_souza_123456@hotmail.com>
2019-11-01 21:29:31 -0300

    commit

---
+++ Exercícios vetores/Exercício 5 - média/Exercício 5 - média/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_5___média
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vetor = new int[11];
+            int i;
+            double    soma=0,media;
+
+
+            for (i = 1; i < vetor.Length; i++)
+            {
+                Console.Write("Digite a {0}º idade: ", i);
+                vetor[i] = int.Parse(Console.ReadLine());
+                soma = vetor[i] + soma;
+                
+                
+            }
+
+            media = soma / i+1;
+
+
+
+            Console.WriteLine("\n média: {0}  ", media);
+
+            Console.ReadKey();
+        }
+    }
+}
