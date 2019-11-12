e9f8c141eb8d995f5f322f9067255bfd3c602c73
Thiagodesouza6 <thiago_souza_123456@hotmail.com>
2019-11-12 19:34:16 -0300

    commit

---
+++ Exercícios vetores/Exercício 7 - Altura/Exercício 7 - Altura/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_7___Altura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] homem = new int[6];
+            int[] mulher = new int[6];
+            int i;
+            int alta = 0,vetor=0,media,soma=0, soma2 = 0,baixo=0;
+
+
+            for (i = 1; i < homem.Length; i++)
+            {
+                Console.Write("Digite a {0}º idade: ", i);
+                homem[i] = int.Parse(Console.ReadLine());
+                soma = homem[i] + soma;
+                if (homem[i] > baixo)
+                {
+                    baixo = homem[i];
+                }
+                else
+                {
+                    vetor = i;
+                }
+                
+            }
+            for (i = 1; i < homem.Length; i++)
+            {
+                Console.Write("\nDigite a {0}º idade: ", i);
+                mulher[i] = int.Parse(Console.ReadLine());
+              
+                if (mulher[i]>alta)
+                {
+                    alta=mulher[i];
+                }
+                soma2 = mulher[i] + soma2;
+            }
+
+            media = (soma + soma2) / 10;
+
+            Console.WriteLine("\n media: {0}  ", media);
+            Console.WriteLine("\n vetor do homem mais baixo: {0}  ", vetor);
+            Console.WriteLine("\n mulher mais alta: {0}  ", alta);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ matriz/matriz/matriz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int LIN = 3, COL = 3;
+            int[,] matriz = new int[LIN, COL];
+            int l, c;
+            for (l = 0; l < LIN; l++)
+            {
+                for (c = 0; c < COL; c++)
+                {
+                    Console.Write("Digite um número: ");
+                    matriz[l, c] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\n Matriz Completa: \n");
+            for (l = 0; l < LIN; l++)
+            {
+                for (c = 0; c < COL; c++)
+                {
+                    Console.Write(" {0} ", matriz[l, c]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n Matriz com Números Pares: \n");
+            for (l = 0; l < LIN; l++)
+            {
+                for (c = 0; c < COL; c++)
+                {
+                    if (matriz[l, c] % 2 == 0)
+                        Console.Write(" {0} ", matriz[l, c]);
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
