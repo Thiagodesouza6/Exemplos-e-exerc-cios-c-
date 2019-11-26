95a285eada9ed9d7684af497883de5205c965ade
Thiagodesouza6 <thiago_souza_123456@hotmail.com>
2019-11-26 20:25:24 -0300

    commit

---
+++ exercícios recuperação/ex2 idade10jogadores/ex2 idade10jogadores/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2_idade10jogadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+            int i, soma = 0,peso,altura,idade,n=0,media;
+            Console.Write("\n");
+            for (i = 0; i < vetor.Length; i++)
+            {
+                Console.Write("\nDigite a idade: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+                soma = vetor[i] + soma;
+                if(vetor[i]<18)
+                {
+                    n++;
+                }
+  
+                }
+            media = soma / 10;
+                Console.WriteLine("idade menor que 18:" + n);
+                Console.WriteLine("média de idade:" + media);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- exercícios recuperação/ex4 - vetor 3x5/ex4 - vetor 3x5/Program.cs
+++ exercícios recuperação/ex4 - vetor 3x5/ex4 - vetor 3x5/Program.cs
@@ -10,30 +10,29 @@ namespace matriz
     {
         static void Main(string[] args)
         {
-           
+
             int[,] matriz = new int[3, 5];
-            int[,] matriz2 = new int[3, 3];
-            int[,] matriz3 = new int[3, 3];
+          
             int l, c;
-            Console.WriteLine("\n Informe os valores da primeira matriz: \n");
+            Console.WriteLine("\n Informe os valores da matriz: \n");
             for (l = 0; l < 3; l++)
             {
                 for (c = 0; c < 5; c++)
                 {
-                    Console.Write("Digite o valor {0}X{1}: ",l + 1, c + 1);
+                    Console.Write("Digite o valor {0},{1}: ", l + 1, c + 1);
                     matriz[l, c] = int.Parse(Console.ReadLine());
                 }
             }
-         
-          
-            Console.WriteLine("\n Soma das matrizes: \n");
+
+
+            Console.WriteLine("\n matriz: \n");
             for (l = 0; l < 3; l++)
             {
                 for (c = 0; c < 5; c++)
                 {
-                
-                        Console.Write(" {0}X{1}: {2} ", l + 1, c + 1,  matriz[l, c]);
-                  
+
+                    Console.Write(" {0},{1}: {2} ", l + 1, c + 1, matriz[l, c]);
+
                 }
                 Console.WriteLine();
             }
