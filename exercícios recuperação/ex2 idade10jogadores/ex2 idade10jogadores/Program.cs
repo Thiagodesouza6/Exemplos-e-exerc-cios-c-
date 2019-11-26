989488cbaa435a08485106f54206aa6f5f41a5c1
Thiagodesouza6 <thiago_souza_123456@hotmail.com>
2019-11-26 20:35:41 -0300

    commit

--- exercícios recuperação/ex2 idade10jogadores/ex2 idade10jogadores/Program.cs
+++ exercícios recuperação/ex2 idade10jogadores/ex2 idade10jogadores/Program.cs
@@ -10,23 +10,38 @@ namespace ex2_idade10jogadores
     {
         static void Main(string[] args)
         {
-            int[] vetor = new int[10];
-            int i, soma = 0,peso,altura,idade,n=0,media;
-            Console.Write("\n");
-            for (i = 0; i < vetor.Length; i++)
+            int[] altura = new int[10];
+            int[] idade = new int[10];
+            int[] peso = new int[10];
+            int i, soma = 0,n=0,media;
+            Console.Write("\nInforme as idades\n");
+            for (i = 0; i < idade.Length; i++)
             {
-                Console.Write("\nDigite a idade: ");
-                vetor[i] = int.Parse(Console.ReadLine());
-                soma = vetor[i] + soma;
-                if(vetor[i]<18)
+                Console.Write("Digite a {0}º idade: ",i + 1);
+                idade[i] = int.Parse(Console.ReadLine());
+                soma = idade[i] + soma;
+                if(idade[i]<18)
                 {
                     n++;
                 }
-  
+               
+
                 }
+            Console.Write("\nInforme os pesos\n");
+            for (i = 0; i < idade.Length; i++)
+            {
+                Console.Write("Digite o {0}º peso: ", i+1);
+                peso[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("\nInforme as alturas\n");
+            for (i = 0; i < altura.Length; i++)
+            {
+                Console.Write("Digite a {0}º altura: ", i + 1);
+                altura[i] = int.Parse(Console.ReadLine());
+            }
             media = soma / 10;
-                Console.WriteLine("idade menor que 18:" + n);
-                Console.WriteLine("média de idade:" + media);
+                Console.WriteLine("\nidade menor que 18 :" + n);
+                Console.WriteLine("\nmédia de idade:" + media);
 
 
 
---
+++ exercícios recuperação/ex5 - transposta/ex5 - transposta/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5___transposta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriznormal = new int[3, 4];
+            int[,] matriztransposta = new int[4, 3];
+            int i, j;
+
+            for (i = 0; i < 3; i++)//leitura da matriz normal
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    Console.Write("[{0},{1}]: ", i + 1, j + 1);
+                    matriznormal[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\n*****Matriz normal 3 X 4******");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    Console.Write("[{0},{1}]: {2} \t", i + 1, j + 1, matriznormal[i, j]);
+                }
+                Console.Write("\n");
+            }
+            for (i = 0; i < 4; i++) //leitura da matriz transposta
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    matriztransposta[i, j] = matriznormal[j, i];
+                }
+
+            }
+            Console.WriteLine("\n*****Matriz transposta 4 X 3******");
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("[{0},{1}]: {2} \t", i + 1, j + 1, matriztransposta[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
