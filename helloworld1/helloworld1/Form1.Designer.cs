580dc9ce0170bb0c98570415f7e16fc55b46ef51
Thiagodesouza6 <thiago_souza_123456@hotmail.com>
2020-02-05 22:22:04 -0300

    commit

---
+++ helloworld1/helloworld1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace helloworld1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 27);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "digite seu nome:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 28);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "CLIQUE AQ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = global::helloworld1.Properties.Resources.image;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(228, 108);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ repeticaotabuada/repeticaotabuada/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repeticaotabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,resultado,x=1,resultado2,y=1,resultado3;
+
+            Console.Write("\n for");
+
+            for (i = 1; i <= 10; i++)
+            {     
+                resultado = i * 5;
+                Console.Write("\n{0}X5={1}", i,resultado);
+            }
+
+            Console.Write("\n");
+            Console.Write("\n do while");
+            do
+            {
+                resultado2 = x * 5;
+                Console.Write("\n{0}X5={1}", x, resultado2);
+                x++;
+            } while (x<= 10);
+
+            Console.Write("\n");
+            Console.Write("\n  while");
+
+            while (y <= 10)
+            {
+                resultado3 = y * 5;
+                Console.Write("\n{0}X5={1}", y, resultado3);
+                y++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
